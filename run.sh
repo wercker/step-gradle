49fc224920979ba272eb4327eb5dd407e2a876cc
Mark Nelson <mark.x.nelson@oracle.com>
2017-10-01 18:29:31 -0400

    no daemon

--- run.sh
+++ run.sh
@@ -116,7 +116,7 @@ export GRADLE_OPTS="$WERCKER_GRADLE_GRADLE_OPTS"
 # run gradle
 #
 export PATH=$PATH:/gradle/gradle-$WERCKER_GRADLE_VERSION/bin
-GRADLE_CMD="gradle $BUILD_FILE $SETTINGS_FILE --console=plain $SYSTEM_PROPS $DEBUG $INIT_SCRIPT $CACHE_DIR --stacktrace --foreground --no-daemon $WERCKER_GRADLE_TASK"
+GRADLE_CMD="gradle $BUILD_FILE $SETTINGS_FILE --console=plain $SYSTEM_PROPS $DEBUG $INIT_SCRIPT $CACHE_DIR --stacktrace --no-daemon $WERCKER_GRADLE_TASK"
 echo "$(date +%H:%M:%S): Running Gradle with command:"
 echo $GRADLE_CMD
 $GRADLE_CMD
